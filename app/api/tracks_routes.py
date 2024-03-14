6bd29d5bf27dd5dc391545a05e4319d5934c02fc
Edward Jung <edwardjung2018@gmail.com>
2024-03-13 19:50:30 -0700

    set up track_blueprint

---
+++ app/api/tracks_routes.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import User
+
+track_routes = Blueprint('tracks', __name__)
+
+
+@track_routes.route('/')
+@login_required
+def users():
+    """
+    Query for all users and returns them in a list of user dictionaries
+    """
+    users = User.query.all()
+    return {'users': [user.to_dict() for user in users]}
+
+
+@track_routes.route('/<int:id>')
+@login_required
+def user(id):
+    """
+    Query for a user by id and returns that user in a dictionary
+    """
+    user = User.query.get(id)
+    return user.to_dict()
