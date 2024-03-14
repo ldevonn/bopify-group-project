97fbb8e06f90793bc54cedf7ad9351a306439ce6
ruokmain <anthony.hoang1991@gmail.com>
2024-03-13 20:16:49 -0700

    completed login route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import User
 
 user_routes = Blueprint('users', __name__)
@@ -14,6 +14,15 @@ def users():
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
 
+@user_routes.route('/current')
+def present_user():
+    """
+    Populates the current user from flask_login package
+    """
+    if current_user.is_anonymous:
+        return {"user": None}
+    return {"user": current_user.to_dict()}
+
 
 @user_routes.route('/<int:id>')
 @login_required
@@ -22,4 +31,6 @@ def user(id):
     Query for a user by id and returns that user in a dictionary
     """
     user = User.query.get(id)
+    if not user:
+        return {'errors': {'message': 'User not found'}}, 404
     return user.to_dict()
