ec42ac018d4d0a704f80eeceb8c0a41c4c929d58
Andrew Streater <streater.andrew@gmail.com>
2024-03-13 19:23:23 -0700

    pushing to dev

--- app/api/albums_routes.py
+++ app/api/albums_routes.py
@@ -8,7 +8,7 @@ album_routes = Blueprint('albums', __name__)
 @album_routes.route('/')
 def get_all_albums():
     albums = Album.query.all()
-    # print(data)
+    print(albums)
     return {'albums': [album.to_dict() for album in albums]}
 
 # Get an Album by albumId
