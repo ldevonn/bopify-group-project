053a04c3ff7c392c7873332c41ded36e76f7f1cb
Edward Jung <edwardjung2018@gmail.com>
2024-03-20 15:45:17 -0700

    updated create track

--- app/api/tracks_routes.py
+++ app/api/tracks_routes.py
@@ -81,6 +81,7 @@ def create_track():
             db.session.add(new_track)
             db.session.commit()
 
-            return redirect(url_for('tracks.get_all_tracks'))
+            return Track.query.filter_by(name=form.name.data).first().to_dict()
+            # return redirect(url_for('tracks.get_all_tracks'))
 
         return render_template('create_track.html', form=form)
