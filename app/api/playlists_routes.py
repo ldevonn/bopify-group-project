c151d74577abc9694cdf6f21ce978de47d1082b1
ruokmain <anthony.hoang1991@gmail.com>
2024-03-21 21:44:15 -0700

    wip put playlist

--- app/api/playlists_routes.py
+++ app/api/playlists_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
-from app.models import Playlist, Track
+from flask import Blueprint, jsonify, request
+from app.models import Playlist, Track, User, db
 from app.models.playlists_tracks import PlaylistsTracks
+from app.forms.playlist_form import PlaylistForm
+
 from flask_login import current_user, login_required
 
 playlist_routes = Blueprint('playlists', __name__)
@@ -19,25 +21,60 @@ def get_current_user_playlists():
 
   return {'playlists': playlist_data}
 
-@playlist_routes.route('/<int:playlist_id>')
+@playlist_routes.route('/<int:playlist_id>', methods=["GET", "PUT", "DELETE"])
 def get_playlist_by_id(playlist_id):
   """
   Query for a playlist by id and returns that playlist in a dictionary including it's tracks
   """
 
   playlist = Playlist.query.get(playlist_id)
-  tracks = Track.query.join(PlaylistsTracks).filter(PlaylistsTracks.columns.playlist_id == playlist_id).all()
   if not playlist:
     res = jsonify({"message": "Playlist couldn't be found"})
     res.status_code = 404
     return res
   
-  return {"playlist": 
-            {
-              "name": playlist.name, 
-              "userId": playlist.user_id,
-              "imageUrl": playlist.image_url,
-              "Private": playlist.private,
-              "tracks": [track.to_dict() for track in tracks]
+  if request.method in ["PUT", "DELETE"] and playlist.user_id != current_user.id:
+    return jsonify({"message": "Unauthorized access"}), 403
+
+  if request.method == "GET":
+    tracks = Track.query.join(PlaylistsTracks).filter(PlaylistsTracks.columns.playlist_id == playlist_id).all()
+    return {"playlist": 
+              {
+                "name": playlist.name, 
+                "userId": playlist.user_id,
+                "imageUrl": playlist.image_url,
+                "Private": playlist.private,
+                "tracks": [track.to_dict() for track in tracks]
+              }
             }
-          }
+  
+  if request.method == "PUT":
+    form = PlaylistForm(obj=playlist)
+    user_playlists = Playlist.query.filter_by(user_id=current_user.id).all()
+    form.user_id.choices = [()]
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+
+
+
+@playlist_routes.route('/new', methods=["POST"])
+@login_required
+def create_playlist():
+  user = User.query.filter_by(id=current_user.id).one().to_dict()
+  if not user:
+    return jsonify({"message": "User not found"})
+  
+  form = PlaylistForm()
+  if form.validate_on_submit():
+    data = form.data
+    new_playlist = Playlist(
+                      name = data["name"],
+                      imageUrl = data["image_url"],
+                      userId = current_user.id,
+                      private = data["private"],
+    )
+    db.session.add(new_playlist)
+    db.session.commit()
+    playlist = Playlist.query.get(new_playlist.id).to_dict()
+    return jsonify(playlist), 201
+  pass
