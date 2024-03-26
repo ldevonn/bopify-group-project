1406d372ef2d3d1e5fcb0e15ce4d85502f762039
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-03-25 22:13:56 -0700

    stuff

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,10 +1,37 @@
 import "./LoginForm.css";
 import spotifyLogo from '../../media/spotifyLogo.png'
-import {Link, useNavigate} from 'react-router-dom'
+import {Navigate, Link, useNavigate} from 'react-router-dom'
+import {useState} from "react";
+import {thunkLogin} from "../../redux/session.js";
+import {useSelector, useDispatch} from "react-redux";
+
+
 
 function LoginFormPage() {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const sessionUser = useSelector((state) => state.session.user)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState({})
 
+    if (sessionUser) return <Navigate to="/" replace={true} />
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const serverResponse = await dispatch(
+            thunkLogin({
+                email,
+                password
+            })
+        )
+        if (serverResponse) {
+            setErrors(serverResponse)
+        } else {
+            navigate('/')
+        }
+    }
   return (
     <>
         <div className='loginPage'>
@@ -14,11 +41,12 @@ function LoginFormPage() {
             <div id='loginFormCard'>
                 <h1 id='loginTitle'>Login to Spotify</h1>
                 <hr className='divider'/>
-                <form id='loginForm'>
+                <form id='loginForm' onSubmit={handleSubmit}>
                     <label style={{background: 'none'}} htmlFor='email'>Email</label>
-                    <input type='email' id='email' name='email' required placeholder='Enter your email'/>
+                    <input type='email' id='email' name='email' required placeholder='Enter your email' onChange={(e) => setEmail(e.target.value)}/>
+                    {errors.length > 0 && errors.map((message) => <p key={message}>{message}</p>)}
                     <label style={{background: 'none'}} htmlFor='password'>Password:</label>
-                    <input type='password' id='password' name='password' required placeholder='Enter your password'/>
+                    <input type='password' id='password' name='password' required placeholder='Enter your password' onChange={(e) => setPassword(e.target.value)}/>
                     <button id='loginSubmit' type='submit'>Login</button>
                 </form>
                 <hr className='divider'/>
