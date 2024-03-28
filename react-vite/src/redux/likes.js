4a1de981941ab556de3b2862675f26eb2b2cc5db
ruokmain <anthony.hoang1991@gmail.com>
2024-03-27 22:25:00 -0700

    completed playlist and likes reducer

---
+++ react-vite/src/redux/likes.js
@@ -0,0 +1,109 @@
+const GET_LIKES = 'likes/getLikes'
+const GET_LIKES_PER_TRACK = 'likes/getLikesPerTrack'
+const LIKED_TRACK = 'likes/likedTrack'
+const DELETE_LIKED = 'likes/deleteLike'
+
+const getLikes = (userLikes) => {
+  return {
+    type: GET_LIKES,
+    userLikes
+  }
+}
+
+const getNumLikesPerTrack = (trackLikes) => {
+  return {
+    type: GET_LIKES_PER_TRACK,
+    trackLikes
+  }
+}
+
+const likedTrack = (trackIsLiked) => {
+  return {
+    type: LIKED_TRACK,
+    trackIsLiked
+  }
+}
+
+const removeLiked = (liked) => {
+  return {
+    type: DELETE_LIKED,
+    liked
+  }
+}
+
+export const fetchGetLikedTracksByCurrentUser = () => async (dispatch) => {
+  const res = await fetch('/api/likes')
+
+  if (res.ok) {
+    const data = await res.json()
+    dispatch(getLikes(data))
+    return data
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const fetchTotalLikesByTrack = (trackId) => async (dispatch) => {
+  const res = await fetch(`/api/likes/${trackId}`)
+  if (res.ok) {
+    const data = await res.json()
+    dispatch(getNumLikesPerTrack(data))
+    return data
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const likeTheTrack = (trackId, payload) => async (dispatch) => {
+  const res = await fetch(`/api/likes/${trackId}`, {
+    method: 'POST',
+    header: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  })
+  if (res.ok) {
+    const data = await res.json()
+    dispatch(likedTrack(data))
+    return data
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const removeLikedTrack = (trackId) => async (dispatch) => {
+  const res = await fetch(`/api/likes/${trackId}`, {
+    method: 'DELETE',
+  })
+  if (res.ok) {
+    dispatch(removeLiked())
+    return res
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+const likesReducer = (state = {}, action) => {
+  switch (action.type) {
+    case GET_LIKES:
+      return { ...state, userLikedTracks: action.userLikes }
+    case GET_LIKES_PER_TRACK:
+      return { ...state, numLikesPerTrack: action.trackLikes }
+    case LIKED_TRACK:
+      return { ...state, trackIsLiked: action.trackIsLiked }
+    default:
+      return state
+  }
+}
+
+export default likesReducer
---
+++ react-vite/src/redux/playlists.js
@@ -0,0 +1,147 @@
+const GET_PLAYLISTS_BY_CURRENT_USER = 'playlists/getPlaylistsByCurrentUser'
+const GET_PLAYLIST_DETAILS = 'playlists/getPlaylistDetails'
+const ADD_PLAYLIST = 'playlists/addPlaylist'
+const UPDATE_PLAYLIST = 'playlists/updatePlaylist'
+const DELETE_PLAYLIST = 'playlists/deletePlaylist'
+
+const getPlaylistsByCurrentUser = (allPlaylists) => {
+  return {
+    type: GET_PLAYLISTS,
+    allPlaylists
+  }
+}
+
+const getPlaylistDetails = (playlistDetails) => {
+  return {
+    type: GET_PLAYLIST_DETAILS,
+    playlistDetails
+  }
+}
+
+const addPlaylist = (newPlaylist) => {
+  return {
+    type: ADD_PLAYLIST,
+    newPlaylist
+  }
+}
+
+const modifiedPlaylist = (updatedPlaylist) => {
+  return {
+    type: UPDATE_PLAYLIST,
+    updatedPlaylist
+  }
+}
+
+const deletePlaylist = (playlist) => {
+  return {
+    type: DELETE_PLAYLIST,
+    playlist
+  }
+}
+
+export const fetchPlaylistByCurrentUser = () => async (dispatch) => {
+  const res = await fetch("api/playlists/current")
+  if (res.ok) {
+    const data = await res.json()
+    const playlistsData = {}
+
+    for (let i = 0; i < data.playlists.length; i++) {
+      let currObj = data.playlists[i]
+      playlistsData[currObj.id] = currObj
+    }
+
+    dispatch(getPlaylistsByCurrentUser(playlistsData))
+
+    return playlistsData
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const fetchGetPlaylistDetails = (playlistId) => async (dispatch) => {
+  const res = await fetch(`/api/playlists/${playlistId}`)
+  if (res.ok) {
+    const data = await res.json()
+    dispatch(getPlaylistDetails(data))
+    return data
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const createPlaylist = (payload) => async (dispatch) => {
+  const res = await fetch(`/api/playlists/new`, {
+    method: 'POST',
+    header: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  })
+
+  if (res.ok) {
+    const data = await res.json()
+    dispatch(addPlaylist(data))
+    return data
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const updatePlaylist = (payload, playlistId) => async (dispatch) => {
+  const res = await fetch(`/api/playlists/${playlistId}`, {
+    method: 'PUT',
+    header: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
+  })
+
+  if (res.ok) {
+    const data = await res.json()
+    dispatch(modifiedPlaylist(data))
+    return data
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+export const fetchDeletePlaylist = (playlistId) => async (dispatch) => {
+  const res = await fetch(`api/playlists/${playlistId}`, {
+    method: 'DELETE'
+  })
+
+  if (res.ok) {
+    dispatch(deletePlaylist())
+    return res
+  } else if (res.status < 500) {
+    const errorMessages = await res.json()
+    return errorMessages
+  } else {
+    return { server: "Something went wrong. Please try again" }
+  }
+}
+
+const playlistsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case GET_PLAYLISTS_BY_CURRENT_USER:
+      return { ...state, allPlaylists: action.allPlaylists }
+    case GET_PLAYLIST_DETAILS:
+      return { ...state, playlistDetails: action.playlistDetails }
+    case ADD_PLAYLIST:
+      return { ...state, playlistDetails: action.newPlaylist }
+    case UPDATE_PLAYLIST:
+      return { ...state, playlistDetails: action.updatedPlaylist }
+    default:
+      return state
+  }
+}
+
+export default playlistsReducer
