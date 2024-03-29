4d8c6781ac56da78529fa1141c9c92dfced1c01c
Andrew Streater <streater.andrew@gmail.com>
2024-03-28 21:33:04 -0700

    album details component completed

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import TopNav from "./TopNav";
 import LeftNav from './LeftNav'
 import TopLeftNav from "./TopLeftNav";
 import AllAlbums from "../AllAlbums";
+import MusicPlayer from "../MusicPlayer/MusicPlayer";
 import "./HomePage.css";
 
 function HomePage() {
@@ -15,6 +16,9 @@ function HomePage() {
                 <div className="top-nav-with-gradient">
                     <TopNav/>
                     <AllAlbums />
+                </div>
+                <div className="music-player">
+                    <MusicPlayer />
                 </div>
             </div>
         </>
