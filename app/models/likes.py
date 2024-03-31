9f8ae4e11d591085ebec60731df154602bc3be55
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-03-31 09:45:43 -0700

    test

--- app/models/likes.py
+++ app/models/likes.py
@@ -1,10 +1,11 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import ForeignKey
 
-Like = db.Table('likes',
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False),
-    db.Column('track_id', db.Integer, db.ForeignKey('tracks.id', ondelete='CASCADE'), nullable=False)
+likes = db.Table('likes',
+    db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), nullable=False),
+    db.Column('track_id', db.Integer, db.ForeignKey('tracks.id'), nullable=False)
 )
 
 if environment == "production":
-    Like.schema = SCHEMA
+    likes.schema = SCHEMA
