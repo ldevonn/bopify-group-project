d19b510543b849a5b2e0d586a1933c1da1e03526
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-03-31 10:33:11 -0700

    changes

--- app/models/playlists_tracks.py
+++ app/models/playlists_tracks.py
@@ -2,13 +2,11 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import ForeignKey
 
 table_name = 'playlists_tracks'
-if environment == "production":
-    table_name = add_prefix_for_prod(table_name)
 
 PlaylistsTracks = db.Table(table_name,
     db.Model.metadata,
-    db.Column('track_id', db.Integer, db.ForeignKey('tracks.id'), nullable=False),
-    db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.id'), nullable=False)
+    db.Column('track_id', db.Integer, db.ForeignKey(add_prefix_for_prod('tracks.id')), nullable=False),
+    db.Column('playlist_id', db.Integer, db.ForeignKey(add_prefix_for_prod('playlists.id')), nullable=False)
 )
 
 if environment == "production":
