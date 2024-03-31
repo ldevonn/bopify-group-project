dc4ea2ef6ca932e3d42b1df61e07449625c29ecc
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-03-31 10:16:31 -0700

    trying

--- app/models/playlists_tracks.py
+++ app/models/playlists_tracks.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import ForeignKey
 
-PlaylistsTracks = db.Table('playliststracks',
+PlaylistsTracks = db.Table('playlists_tracks',
     db.Model.metadata,
     db.Column('track_id', db.Integer, db.ForeignKey('tracks.id'), nullable=False),
     db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.id'), nullable=False)
