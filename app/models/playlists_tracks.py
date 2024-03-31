12ee1c4a6741b884c28ed6884307bca860936f46
Lars-Devon Nilsson <larsdevonn@gmail.com>
2024-03-31 10:06:55 -0700

    testing

--- app/models/playlists_tracks.py
+++ app/models/playlists_tracks.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy import ForeignKey
 
-PlaylistsTracks = db.Table('playlistsTracks',
+PlaylistsTracks = db.Table('playliststracks',
     db.Model.metadata,
     db.Column('track_id', db.Integer, db.ForeignKey('tracks.id'), nullable=False),
     db.Column('playlist_id', db.Integer, db.ForeignKey('playlists.id'), nullable=False)
@@ -9,17 +9,3 @@ PlaylistsTracks = db.Table('playlistsTracks',
 
 if environment == "production":
     PlaylistsTracks.schema = SCHEMA
-    
-    
-
-    # def to_dict(self):
-    #     return {
-    #         'id': self.id,
-    #         'trackId': self.track_id,
-    #         'playlistId': self.playlist_id
-    #     }
-
-# class Child(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     parent_id = db.Column(db.Integer, ForeignKey('parent.id', ondelete='CASCADE'))
-#     parent = relationship("Parent", back_populates="children")
